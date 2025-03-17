8f309f4f0dc34be225f790f8cd9f5b564feb2d3c
andres.mera <feli041089@gmail.com>
2025-03-17 14:03:20 -0500

    code 01: creation of the external entities of the system.

---
+++ src/main/java/com/book_external/app/model/external/Geo.java
@@ -0,0 +1,13 @@
+package com.book_external.app.model.external;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class Geo {
+    private String lat;
+    private String lng;
+}
