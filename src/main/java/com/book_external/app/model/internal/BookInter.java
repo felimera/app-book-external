bee28e4e91ab3a5b936cb935aa85bc0258c75bde
andres.mera <feli041089@gmail.com>
2025-03-19 16:24:32 -0500

    code 03: Modification in the entity mapping.

--- src/main/java/com/book_external/app/model/internal/BookInter.java
+++ src/main/java/com/book_external/app/model/internal/BookInter.java
@@ -1,9 +1,6 @@
 package com.book_external.app.model.internal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -12,5 +9,6 @@ import lombok.Data;
 public class BookInter {
     @Id
     @Column(name = "boo_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
 }
