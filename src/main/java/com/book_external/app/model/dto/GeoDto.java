cafa5995a71e29f1eb4f9cf5a1417524a19753fe
andres.mera <feli041089@gmail.com>
2025-03-19 15:37:24 -0500

    code 03: Creation of DTOs for the client's external consultation.

---
+++ src/main/java/com/book_external/app/model/dto/GeoDto.java
@@ -0,0 +1,13 @@
+package com.book_external.app.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GeoDto {
+    private String latitud;
+    private String longitud;
+}
