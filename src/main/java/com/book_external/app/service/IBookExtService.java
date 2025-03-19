6ee2ecf8ebdee4eb74cdcfd5b045c4633eebbd2d
andres.mera <feli041089@gmail.com>
2025-03-19 14:41:34 -0500

    code 03: Creation of the external book consumption service.

---
+++ src/main/java/com/book_external/app/service/IBookExtService.java
@@ -0,0 +1,9 @@
+package com.book_external.app.service;
+
+import com.book_external.app.model.dto.BookExtDto;
+
+import java.util.List;
+
+public interface IBookExtService {
+    List<BookExtDto> getListAll();
+}
