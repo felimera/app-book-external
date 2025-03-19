c44c7f9ba83170684db6ab05b8a62d6163f71c0d
andres.mera <feli041089@gmail.com>
2025-03-19 09:24:45 -0500

    code 03: Create the category creation and editing endpoints.

--- src/main/java/com/book_external/app/controller/CategoryController.java
+++ src/main/java/com/book_external/app/controller/CategoryController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,14 +26,28 @@ public class CategoryController {
     @Operation(summary = "Get a list of all registered categories with subcategories.")
     @PostMapping(path = "/subcategories")
     public ResponseEntity<List<CategoryDto>> getCategoryListByMultipleParameter(@RequestBody CategoryDto dto) {
-        log.info("Dto =>>" + dto.toString());
+        log.info("Dto subcategories =>>" + dto.toString());
         return ResponseEntity.ok(iCategoryService.getCategoryListByMultipleParameter(dto));
     }
 
     @Operation(summary = "Get a list of all categories by name and/or description.")
     @PostMapping(path = "/namedescription")
     public ResponseEntity<List<CategoryDto>> getCategoryByNameOrDescription(@RequestBody CategoryDto dto) {
-        log.info("Dto =>>" + dto.toString());
+        log.info("Dto namedescription =>>" + dto.toString());
         return ResponseEntity.ok(iCategoryService.getCategoryByNameOrDescription(dto));
+    }
+
+    @Operation(summary = "Create a Category record.")
+    @PostMapping
+    public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto dto) {
+        log.info("Dto post =>>" + dto.toString());
+        return ResponseEntity.ok(iCategoryService.create(dto));
+    }
+
+    @Operation(summary = "Modify a Category record.")
+    @PutMapping(path = "{id}")
+    public ResponseEntity<CategoryDto> edit(@PathVariable(name = "id") Integer id, @RequestBody CategoryDto dto) {
+        log.info("Dto put =>>" + dto.toString());
+        return ResponseEntity.ok(iCategoryService.edit(id, dto));
     }
 }
